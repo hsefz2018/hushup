5ac750478332179bd758a5f8cc2ccf5ee83bc5e9
Pisces000221 <1786762946@qq.com>
2016-07-31 15:19:39 +0800

    Add lemon checker for STUMBLER

---
+++ STUMBLER/judger/checker-lemon.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cassert>
+#include <cmath>
+
+template <typename T> inline T min(const T a, const T b) {
+    return a < b ? a : b;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *out = fopen(argv[2], "r");
+    FILE *ans = fopen(argv[3], "r");
+    freopen(argv[5], "w", stdout);
+    freopen(argv[6], "w", stderr);
+
+    double jury_ans, ctst_ans;
+    assert(fscanf(ans, "%lf", &jury_ans) == 1);
+    if (fscanf(out, "%lf", &ctst_ans) != 1) {
+        fprintf(stderr, "Incorrect format: must contain one decimal number");
+        printf("0.0");
+    } else {
+        if (fabs(jury_ans - ctst_ans) > 1e-6) {
+            fprintf(stderr, "Incorrect answer: absolute error is %.2e", fabs(jury_ans - ctst_ans));
+            printf("0.0");
+        } else {
+            fprintf(stderr, "Correct (・ω・)ノ");
+            printf("1.0");
+        }
+    }
+
+    fclose(out); fclose(ans);
+    fclose(stdout); fclose(stderr);
+    return 0;
+}
+
