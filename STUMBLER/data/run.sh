d2726236433dd38acad0a70c25bec707ccfbb1bd
Pisces000221 <1786762946@qq.com>
2016-07-31 12:36:30 +0800

    Add run.sh to STUMBLER

---
+++ STUMBLER/data/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for i in {1..5}
+do
+    for j in {1..9}
+    do
+        echo "$i-0$j"
+        cat tests/$i-0$j.in | head -n 1
+        ./a.out < tests/$i-0$j.in > tests/$i-0$j.ans
+    done
+    echo "$i-10"
+    cat tests/$i-10.in | head -n 1
+    ./a.out < tests/$i-10.in > tests/$i-10.ans
+done
+
