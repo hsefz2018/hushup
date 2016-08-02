ce0ad205ffc032540a0096cd9b9ee2a8e9239e42
Pisces000221 <1786762946@qq.com>
2016-08-02 11:55:27 +0800

    Finalization. We're ready!

--- CACCEPT/dist/caccept.pas
+++ CACCEPT/dist/caccept.pas
@@ -12,8 +12,6 @@ Procedure newGame;
 Var
     a, b, c, d, e: Char;
 Begin
-    {For a := 'a' to 'z' Do inc(b);}
-    {WriteLn(makeAttempt('a', 'b', 'c', 'd', 'e'));}
     For a := 'a' to 'z' Do
         For b := 'a' to 'z' Do
             For c := 'a' to 'z' Do
